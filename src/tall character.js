db185276325a6a3298839f4a92898f8dd1f00416
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-07-01 10:17:04 -0400

    autolint

--- src/tall character.js
+++ src/tall character.js
@@ -16,25 +16,22 @@ Add a "graphic" tag as well.
 export const hackOptions = {};
 
 const directions = {
-	'up': [0, -1],
-	'dowm': [0, 1],
-	'left': [-1, 0],
-	'right': [1, 0],
-}
+	up: [0, -1],
+	dowm: [0, 1],
+	left: [-1, 0],
+	right: [1, 0],
+};
 
 hackOptions.direction = directions[FIELD(CONFIG, 'direction', 'text') ?? 'up'];
 
 wrap.before(BipsiPlayback.prototype, 'render', () => {
 	const playback = window.PLAYBACK;
 	const avatar = getEventById(playback.data, playback.avatarId);
-	const head = findEventByTag(playback.data, "is-head");
+	const head = findEventByTag(playback.data, 'is-head');
 	const pos = getLocationOfEvent(playback.data, avatar);
 	const new_pos = {
-		"room": pos.room,
-		"position": [
-			pos.position[0] + hackOptions.direction[0],
-			pos.position[1] + hackOptions.direction[1],
-		]
+		room: pos.room,
+		position: [pos.position[0] + hackOptions.direction[0], pos.position[1] + hackOptions.direction[1]],
 	};
 	moveEvent(playback.data, head, new_pos);
 });
