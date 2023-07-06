92fc7bb8ae6a43ba8335b2580f6c25727fbe3138
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-07-06 18:44:38 -0400

    autoformat

--- src/mirrored-event-graphics.js
+++ src/mirrored-event-graphics.js
@@ -17,50 +17,39 @@ HOW TO USE:
 4. Run the game and note that the event now shows up mirrored.
 */
 
-drawEventLayer = function(destination, tileset, tileToFrame, palette, events) {
-    drawRecolorLayer(destination, (backg, color, tiles) => {
-        events.forEach((event) => {
-            const [tx, ty] = event.position;
-            const graphicField = oneField(event, "graphic", "tile");
-            if (graphicField) {
-                let { fg, bg } = FIELD(event, "colors", "colors") ?? { bg: 1, fg: 3 };
+drawEventLayer = function (destination, tileset, tileToFrame, palette, events) {
+	drawRecolorLayer(destination, (backg, color, tiles) => {
+		events.forEach(event => {
+			const [tx, ty] = event.position;
+			const graphicField = oneField(event, 'graphic', 'tile');
+			if (graphicField) {
+				let { fg, bg } = FIELD(event, 'colors', 'colors') ?? { bg: 1, fg: 3 };
 
-                const frameIndex = tileToFrame.get(graphicField.data) ?? 0;
-                const { x, y, size } = getTileCoords(tileset.canvas, frameIndex);
+				const frameIndex = tileToFrame.get(graphicField.data) ?? 0;
+				const { x, y, size } = getTileCoords(tileset.canvas, frameIndex);
 
-                if (eventIsTagged(event, "transparent")) {
-                    bg = 0;
-                }
+				if (eventIsTagged(event, 'transparent')) {
+					bg = 0;
+				}
 
-                if (bg > 0) {
-                    backg.fillStyle = palette.colors[bg];
-                    backg.fillRect(tx * size, ty * size, size, size);
-                }
+				if (bg > 0) {
+					backg.fillStyle = palette.colors[bg];
+					backg.fillRect(tx * size, ty * size, size, size);
+				}
 
-                if (fg > 0) {
-                    color.fillStyle = palette.colors[fg];
-                    color.fillRect(tx * size, ty * size, size, size);
-                }
+				if (fg > 0) {
+					color.fillStyle = palette.colors[fg];
+					color.fillRect(tx * size, ty * size, size, size);
+				}
 
-                if (FIELD(event, "mirrored", "tag"))
-                {
-                    tiles.scale(-1,1);
-                    tiles.drawImage(
-                        tileset.canvas,
-                        x, y, size, size,
-                        -tx * size, ty * size, -size, size,
-                    );
-                    tiles.scale(-1,1);
-                }
-                else
-                {
-                    tiles.drawImage(
-                        tileset.canvas,
-                        x, y, size, size,
-                        tx * size, ty * size, size, size,
-                    );
-                }
-            }
-        });
-    });
-}
+				if (FIELD(event, 'mirrored', 'tag')) {
+					tiles.scale(-1, 1);
+					tiles.drawImage(tileset.canvas, x, y, size, size, -tx * size, ty * size, -size, size);
+					tiles.scale(-1, 1);
+				} else {
+					tiles.drawImage(tileset.canvas, x, y, size, size, tx * size, ty * size, size, size);
+				}
+			}
+		});
+	});
+};
--- src/speech-portraits-from-tiles.js
+++ src/speech-portraits-from-tiles.js
@@ -48,8 +48,12 @@ if (window.portraitVars) {
 	console.error('portraitVars over-defined.  Suggests multiple copies of the same plugin.');
 }
 window.portraitVars = {
-	currentPortraitId: -1, currentSide: 0, currentFgColorIndex: 3, currentBgColorIndex: 1,
-	currentBorderColorIndex: 0, currentEvent: null
+	currentPortraitId: -1,
+	currentSide: 0,
+	currentFgColorIndex: 3,
+	currentBgColorIndex: 1,
+	currentBorderColorIndex: 0,
+	currentEvent: null,
 };
 portraitVars.OFFSET_X_LEFT = 24;
 portraitVars.OFFSET_X_RIGHT = 232;
@@ -59,23 +63,22 @@ portraitVars.TINT_CANVAS.width = 8;
 portraitVars.TINT_CANVAS.height = 8;
 portraitVars.TINT_CANVAS_CONTEXT = portraitVars.TINT_CANVAS.getContext('2d');
 portraitVars.SCALE = parseInt(FIELD(CONFIG, 'scale', 'text')) || 5;
-portraitVars.DEFAULT_BORDER_PALETTE_COLOR =
-	parseInt(FIELD(CONFIG, 'default-border-palette-color', 'text')) || 0;
-if (portraitVars.DEFAULT_BORDER_PALETTE_COLOR <0 || portraitVars.DEFAULT_BORDER_PALETTE_COLOR >7) {
+portraitVars.DEFAULT_BORDER_PALETTE_COLOR = parseInt(FIELD(CONFIG, 'default-border-palette-color', 'text')) || 0;
+if (portraitVars.DEFAULT_BORDER_PALETTE_COLOR < 0 || portraitVars.DEFAULT_BORDER_PALETTE_COLOR > 7) {
 	portraitVars.DEFAULT_BORDER_PALETTE_COLOR = 0;
 }
 portraitVars.DEFAULT_SIDE = FIELD(CONFIG, 'default-side', 'text');
 if (portraitVars.DEFAULT_SIDE != 0 && portraitVars.DEFAULT_SIDE != 1) {
-	portraitVars.DEFAULT_SIDE = 0
+	portraitVars.DEFAULT_SIDE = 0;
 }
 
-///////////////////////////////////
+/// ////////////////////////////////
 // TRACK THE CURRENTLY RUN EVENT //
-///////////////////////////////////
+/// ////////////////////////////////
 if (!portraitVars.orig_bipsiPlayback_runJS) {
 	portraitVars.orig_bipsiPlayback_runJS = BipsiPlayback.prototype.runJS;
 }
-BipsiPlayback.prototype.runJS = async function(event, js, debug=false) {
+BipsiPlayback.prototype.runJS = async function (event, js, debug = false) {
 	// Original logic
 	portraitVars.orig_bipsiPlayback_runJS.bind(this)(event, js, debug);
 
@@ -83,13 +86,13 @@ BipsiPlayback.prototype.runJS = async function(event, js, debug=false) {
 	portraitVars.currentEvent = event;
 };
 
-///////////////////////////////////
+/// ////////////////////////////////
 // PROCESS TEXT FOR PORTRAIT IDS //
-///////////////////////////////////
+/// ////////////////////////////////
 if (!portraitVars.orig_parseFakeDown) {
 	portraitVars.orig_parseFakeDown = parseFakedown;
 }
-parseFakedown = function(text) {
+parseFakedown = function (text) {
 	// Original logic
 	text = portraitVars.orig_parseFakeDown(text);
 
@@ -101,7 +104,7 @@ parseFakedown = function(text) {
 if (!portraitVars.orig_dialoguePlayback_applyStyle) {
 	portraitVars.orig_dialoguePlayback_applyStyle = DialoguePlayback.prototype.applyStyle;
 }
-DialoguePlayback.prototype.applyStyle = function() {
+DialoguePlayback.prototype.applyStyle = function () {
 	// Original logic
 	portraitVars.orig_dialoguePlayback_applyStyle.bind(this)();
 
@@ -109,36 +112,32 @@ DialoguePlayback.prototype.applyStyle = function() {
 	this.currentPage.glyphs.forEach((glyph, i) => {
 		if (glyph.styles.has('portrait')) {
 			const args = glyph.styles.get('portrait').split(',');
-			portraitVars.currentPortraitId = parseInt(args[0])
+			portraitVars.currentPortraitId = parseInt(args[0]);
 			portraitVars.currentSide = parseInt(args[1]);
 			portraitVars.currentFgColorIndex = parseInt(args[2]);
 			portraitVars.currentBgColorIndex = parseInt(args[3]);
 			portraitVars.currentBorderColorIndex = parseInt(args[4]);
 			// Use the portraited event's colors as defaults.
-			const eventColors = FIELD(portraitVars.currentEvent, 'colors', 'colors') || {fg:3,bg:1};
-			if ((!portraitVars.currentPortraitId && portraitVars.currentPortraitId != 0) ||
-			    portraitVars.currentPortraitId < -1) {
+			const eventColors = FIELD(portraitVars.currentEvent, 'colors', 'colors') || { fg: 3, bg: 1 };
+			if ((!portraitVars.currentPortraitId && portraitVars.currentPortraitId != 0) || portraitVars.currentPortraitId < -1) {
 				portraitVars.currentPortraitId = -1; // -1 = NO portrait shown
 			}
 			if (portraitVars.currentSide != 0 && portraitVars.currentSide != 1) {
 				portraitVars.currentSide = portraitVars.DEFAULT_SIDE;
 			}
-			if ((!portraitVars.currentFgColorIndex && portraitVars.currentFgColorIndex != 0) ||
-			    portraitVars.currentFgColorIndex < 0 || portraitVars.currentFgColorIndex > 7) {
+			if ((!portraitVars.currentFgColorIndex && portraitVars.currentFgColorIndex != 0) || portraitVars.currentFgColorIndex < 0 || portraitVars.currentFgColorIndex > 7) {
 				portraitVars.currentFgColorIndex = eventColors.fg;
 			}
-			if ((!portraitVars.currentBgColorIndex && portraitVars.currentBgColorIndex != 0) ||
-			    portraitVars.currentBgColorIndex < 0 || portraitVars.currentBgColorIndex > 7) {
+			if ((!portraitVars.currentBgColorIndex && portraitVars.currentBgColorIndex != 0) || portraitVars.currentBgColorIndex < 0 || portraitVars.currentBgColorIndex > 7) {
 				portraitVars.currentBgColorIndex = eventColors.bg;
 			}
-			if ((!portraitVars.currentBorderColorIndex && portraitVars.currentBorderColorIndex != 0) ||
-			    portraitVars.currentBorderColorIndex < 0 || portraitVars.currentBorderColorIndex > 7) {
+			if ((!portraitVars.currentBorderColorIndex && portraitVars.currentBorderColorIndex != 0) || portraitVars.currentBorderColorIndex < 0 || portraitVars.currentBorderColorIndex > 7) {
 				portraitVars.currentBorderColorIndex = portraitVars.DEFAULT_BORDER_PALETTE_COLOR;
 			}
 		}
 	});
 };
-portraitVars.portraitFakedownToTag = function(text) {
+portraitVars.portraitFakedownToTag = function (text) {
 	// Make sure the "@@"s are properly paired
 	if ((text.match(/@@/g) ?? []).length % 2 != 0) {
 		return text;
@@ -148,28 +147,26 @@ portraitVars.portraitFakedownToTag = function(text) {
 	let start = text.indexOf('@@');
 	let end = 0;
 	while (start != -1) {
-		end = text.indexOf('@@', start+1);
-		text =
-			text.slice(0, start) + '{portrait=' + text.slice(start+2, end) + '}' +
-			text.slice(end+2, end+3) + '{-portrait}' + text.slice(end+3);
+		end = text.indexOf('@@', start + 1);
+		text = `${text.slice(0, start)}{portrait=${text.slice(start + 2, end)}}${text.slice(end + 2, end + 3)}{-portrait}${text.slice(end + 3)}`;
 		start = text.indexOf('@@');
 	}
 
 	// If the first character does not already have a portrait style, add a NON-portrait style.
 	if (!text.startsWith('{portrait=')) {
-		text = '{portrait=-1}' + text[0] + '{-portrait}' + text.slice(1);
+		text = `{portrait=-1}${text[0]}{-portrait}${text.slice(1)}`;
 	}
 
 	return text;
 };
 
-///////////////////
+/// ////////////////
 // DRAW PORTRAIT //
-///////////////////
+/// ////////////////
 if (!portraitVars.orig_dialoguePlayback_render) {
 	portraitVars.orig_dialoguePlayback_render = DialoguePlayback.prototype.render;
 }
-DialoguePlayback.prototype.render = function() {
+DialoguePlayback.prototype.render = function () {
 	// No portrait? do original logic only
 	if (portraitVars.currentPortraitId < 0) {
 		portraitVars.orig_dialoguePlayback_render.bind(this)();
@@ -183,60 +180,51 @@ DialoguePlayback.prototype.render = function() {
 	portraitVars.orig_dialoguePlayback_render.bind(this)();
 
 	// Determine where to draw the portrait AND with what colors
-	const portraitLoc = [
-		portraitVars.currentSide == 1 ?
-			(portraitVars.OFFSET_X_RIGHT - 12 * portraitVars.SCALE) :
-			(portraitVars.OFFSET_X_LEFT),
-		(portraitVars.OFFSET_Y - 12 * portraitVars.SCALE)
-	];
+	const portraitLoc = [portraitVars.currentSide == 1 ? portraitVars.OFFSET_X_RIGHT - 12 * portraitVars.SCALE : portraitVars.OFFSET_X_LEFT, portraitVars.OFFSET_Y - 12 * portraitVars.SCALE];
 	const options = this.getOptions(this.currentPage.options);
 	const palette = PLAYBACK.getActivePalette();
-	const borderColor =
-		portraitVars.currentBorderColorIndex == 0 ?
-		options.panelColor :
-		palette.colors[portraitVars.currentBorderColorIndex];
+	const borderColor = portraitVars.currentBorderColorIndex == 0 ? options.panelColor : palette.colors[portraitVars.currentBorderColorIndex];
 
 	// Draw a panel border
 	this.dialogueRendering.fillStyle = borderColor;
-	this.dialogueRendering.fillRect(
-		portraitLoc[0] + 0 * portraitVars.SCALE, portraitLoc[1] + 0 * portraitVars.SCALE,
-		12 * portraitVars.SCALE, 12 * portraitVars.SCALE);
+	this.dialogueRendering.fillRect(portraitLoc[0] + 0 * portraitVars.SCALE, portraitLoc[1] + 0 * portraitVars.SCALE, 12 * portraitVars.SCALE, 12 * portraitVars.SCALE);
 
 	// Draw a background rectangle behind the portrait
-	if (portraitVars.currentBgColorIndex > 0)
-	{
+	if (portraitVars.currentBgColorIndex > 0) {
 		this.dialogueRendering.fillStyle = palette.colors[portraitVars.currentBgColorIndex];
-		this.dialogueRendering.fillRect(
-			portraitLoc[0] + 1 * portraitVars.SCALE, portraitLoc[1] + 1 * portraitVars.SCALE,
-			10 * portraitVars.SCALE, 10 * portraitVars.SCALE);
+		this.dialogueRendering.fillRect(portraitLoc[0] + 1 * portraitVars.SCALE, portraitLoc[1] + 1 * portraitVars.SCALE, 10 * portraitVars.SCALE, 10 * portraitVars.SCALE);
 	}
 
 	// Draw the portrait
-	if (portraitVars.currentFgColorIndex > 0)
-	{
+	if (portraitVars.currentFgColorIndex > 0) {
 		// Draw the portrait from frame-canvas to tint-canvas (flipped if necessary)
 		const frameCanvas = PLAYBACK.stateManager.resources.get(PLAYBACK.data.tileset).canvas;
 		const tileFrameIds = PLAYBACK.data.tiles[portraitVars.currentPortraitId - 1].frames;
 		const animationStep = PLAYBACK.frameCount % tileFrameIds.length;
 		const frameId = tileFrameIds[animationStep];
 		portraitVars.TINT_CANVAS_CONTEXT.globalCompositeOperation = 'source-over';
-		portraitVars.TINT_CANVAS_CONTEXT.fillStyle =
-			palette.colors[portraitVars.currentFgColorIndex];
-		portraitVars.TINT_CANVAS_CONTEXT.fillRect(
-			0, 0, portraitVars.TINT_CANVAS.width, portraitVars.TINT_CANVAS.height);
+		portraitVars.TINT_CANVAS_CONTEXT.fillStyle = palette.colors[portraitVars.currentFgColorIndex];
+		portraitVars.TINT_CANVAS_CONTEXT.fillRect(0, 0, portraitVars.TINT_CANVAS.width, portraitVars.TINT_CANVAS.height);
 		portraitVars.TINT_CANVAS_CONTEXT.globalCompositeOperation = 'destination-atop';
 		if (portraitVars.currentSide == 1) {
 			portraitVars.TINT_CANVAS_CONTEXT.scale(-1, 1);
-			portraitVars.TINT_CANVAS_CONTEXT.drawImage(frameCanvas, frameId*8,0,8,8, 0,0,-8,8);
+			portraitVars.TINT_CANVAS_CONTEXT.drawImage(frameCanvas, frameId * 8, 0, 8, 8, 0, 0, -8, 8);
 			portraitVars.TINT_CANVAS_CONTEXT.scale(-1, 1);
 		} else {
-			portraitVars.TINT_CANVAS_CONTEXT.drawImage(frameCanvas, frameId*8,0,8,8, 0,0,8,8);
+			portraitVars.TINT_CANVAS_CONTEXT.drawImage(frameCanvas, frameId * 8, 0, 8, 8, 0, 0, 8, 8);
 		}
 
 		// Draw the portrait from tint-canvas to screen
 		this.dialogueRendering.drawImage(
-			portraitVars.TINT_CANVAS, 0, 0, 8, 8,
-			portraitLoc[0] + 2 * portraitVars.SCALE, portraitLoc[1] + 2 * portraitVars.SCALE,
-			8 * portraitVars.SCALE,  8  * portraitVars.SCALE);
+			portraitVars.TINT_CANVAS,
+			0,
+			0,
+			8,
+			8,
+			portraitLoc[0] + 2 * portraitVars.SCALE,
+			portraitLoc[1] + 2 * portraitVars.SCALE,
+			8 * portraitVars.SCALE,
+			8 * portraitVars.SCALE
+		);
 	}
 };
