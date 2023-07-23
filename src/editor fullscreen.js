4e566d057f809238d5974b8aa87bb4d6218da3e9
Jonathan Heard <jon.heard.tutor@gmail.com>
2023-07-23 15:00:15 -0400

    Code polish - attached function to class, not instance.

--- src/editor fullscreen.js
+++ src/editor fullscreen.js
@@ -29,7 +29,7 @@ NOTE: if this plugin's configuration is changed, the key to press may not be 'en
 
 function setupEditorPlugin() {
 	// Fullscreen toggle logic
-	window.EDITOR.toggleFullscreen = function toggleFullscreen() {
+	BipsiEditor.prototype.toggleFullscreen = function toggleFullscreen() {
 		const canvas = this.playtestIframe.contentWindow.document.getElementById('player-canvas');
 		if (!canvas) return;
 		if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
@@ -64,7 +64,7 @@ function setupEditorPlugin() {
 	fullscreenButton.classList.add('icon-button');
 	fullscreenButton.title = 'toggle fullscreen';
 	fullscreenButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewbox="0 0 16 16" stroke="currentColor" stroke-linecap="round" fill="none" stroke-width="1.25"><path d="M5 2h-3v3"/><path d="M11 2h3v3"/><path d="M2 11v3h3"/><path d="M14 11v3h-3"/></svg>`;
-	fullscreenButton.addEventListener('click', window.EDITOR.toggleFullscreen.bind(window.EDITOR));
+	fullscreenButton.addEventListener('click', BipsiEditor.prototype.toggleFullscreen.bind(window.EDITOR));
 
 	// Hotkey
 	document.addEventListener('keydown', e => {
