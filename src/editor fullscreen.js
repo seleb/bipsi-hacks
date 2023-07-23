dae5c23275817cf591d9dbfe607146da5ba8780d
Jon Heard <jon.heard.tutor@gmail.com>
2023-07-23 17:47:22 -0400

    feat: add `editor fullscreen` plugin (#14)
    This is a pretty basic plugin, and an update to something you saw
    earlier. It's an editor plugin, which Candle just added support for in
    Bipsi. I don't know if you want to separate editor plugins from game
    plugins in the list. It's not necessarily a binary designation, though I
    suspect most plugins will either be "editor" or "game".
    
    In case you aren't familiar with the editor plugin system:
    - Any code following `//! CODE_RUNTIME`, or any code that doesn't foll
    under one of these special comments (all plugins up till now) will be
    handled like normal plugin code: added to the game in the editor's
    playtest mode AND added to the game when exported.
    - Any code following `//! CODE_EDITOR` is run ONLY in the editor: (1)
    when the plugin is first added (2) when the app is first loaded and (3)
    when a game is imported.
    - Any code following `//! CODE_RUNTIME_DEV` is added to the game, but
    only in the editor's playtest mode: it's NOT added to the game when
    exported.
    
    FYI, I've got 3 other editor plugins that are waiting on code-polish
    before submitting to you.
    
    I hope you don't mind all these new submissions I'm sending your way.
    
    ---------
    
    Co-authored-by: Sean S. LeBlanc <sean.s.leblanc@gmail.com>

---
+++ src/editor fullscreen.js
@@ -0,0 +1,92 @@
+/**
+🖥️
+@file editor fullscreen
+@summary Add a ui button and hotkey to the editor to toggle fullscreen while playtesting the game.
+@license MIT
+@author Violgamba (Jon Heard)
+
+@description
+Add a ui button and hotkey to the editor to toggle fullscreen mode while playtesting the game.
+If 'gamepad-input' plugin is used, a gamepad's 'start' button triggers the 'enter' key.  This is
+why 'enter' is the default fullscreen hotkey, to allow toggling fullscreen from a gamepad.
+
+
+HOW TO USE:
+1. Import this plugin into your game.
+2. Run the game.
+3. Press the 'enter' key.  Note that the game becomes fullscreen.
+4. Press the 'enter' key again.  Note that the game becomes non-fullscreen.
+5. Find the fullscreen button on the lower left of the ui (while the game is running).
+6. Press the fullscreen button.  Note that the game becomes fullscreen.
+NOTE: if this plugin's configuration is changed, the key to press may not be 'enter'.
+
+
+// Which keyboard key triggers fullscreen
+//!CONFIG fullscreen-hotkey (text) "Enter"
+*/
+
+//! CODE_EDITOR
+
+function setupEditorPlugin() {
+	// Fullscreen toggle logic
+	BipsiEditor.prototype.toggleFullscreen = function toggleFullscreen() {
+		const canvas = this.playtestIframe.contentWindow.document.getElementById('player-canvas');
+		if (!canvas) return;
+		if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+			if (canvas.requestFullscreen) {
+				canvas.requestFullscreen();
+			} else if (document.documentElement.msRequestFullscreen) {
+				canvas.msRequestFullscreen();
+			} else if (document.documentElement.mozRequestFullScreen) {
+				canvas.mozRequestFullScreen();
+			} else if (document.documentElement.webkitRequestFullscreen) {
+				canvas.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+			}
+		} else {
+			// eslint-disable-next-line no-lonely-if
+			if (document.exitFullscreen) {
+				document.exitFullscreen();
+			} else if (document.msExitFullscreen) {
+				document.msExitFullscreen();
+			} else if (document.mozCancelFullScreen) {
+				document.mozCancelFullScreen();
+			} else if (document.webkitExitFullscreen) {
+				document.webkitExitFullscreen();
+			}
+		}
+	};
+
+	// Playtest toolbar button
+	const playtestToolbar = document.getElementById('play-tab-view').children[1];
+	const fullscreenButton = document.createElement('button');
+	playtestToolbar.prepend(fullscreenButton);
+	fullscreenButton.id = 'fullscreenButton';
+	fullscreenButton.classList.add('icon-button');
+	fullscreenButton.title = 'toggle fullscreen';
+	fullscreenButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewbox="0 0 16 16" stroke="currentColor" stroke-linecap="round" fill="none" stroke-width="1.25"><path d="M5 2h-3v3"/><path d="M11 2h3v3"/><path d="M2 11v3h3"/><path d="M14 11v3h-3"/></svg>`;
+	fullscreenButton.addEventListener('click', BipsiEditor.prototype.toggleFullscreen.bind(window.EDITOR));
+
+	// Hotkey
+	document.addEventListener('keydown', e => {
+		if (e.key === (FIELD(CONFIG, 'fullscreen-hotkey', 'text').trim() || 'Enter')) {
+			window.EDITOR.toggleFullscreen();
+		}
+	});
+
+	// Prevent repeating this setup
+	window.EDITOR.loadedEditorPlugins ||= new Set();
+	window.EDITOR.loadedEditorPlugins.add('fullscreen');
+}
+if (!window.EDITOR.loadedEditorPlugins?.has('fullscreen')) {
+	setupEditorPlugin();
+}
+
+//! CODE_PLAYBACK_DEV
+
+// Add fullscreen hotkey while the playtest canvas has the focused (i.e. while playtesting the game)
+const FULLSCREEN_HOTKEY = FIELD(CONFIG, 'fullscreen-hotkey', 'text').trim() || 'Enter';
+document.addEventListener('keydown', e => {
+	if (e.key === FULLSCREEN_HOTKEY) {
+		window.parent.window.EDITOR.toggleFullscreen();
+	}
+});
