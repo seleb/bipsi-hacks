a27a1d57a826b526cdcf6f8dcfb6fe4251fc50ce
Jon Heard <jon.heard.tutor@gmail.com>
2023-08-08 00:20:50 -0400

    Made button styling into a full style element. Made button styling customizable in the plugin.

--- src/fullscreen button.js
+++ src/fullscreen button.js
@@ -4,7 +4,6 @@
 @summary Adds a fullscreen button to the lower-right of the game canvas.
 @license MIT
 @author Violgamba (Jon Heard)
-@version 4.7.0
 
 
 @description
@@ -17,26 +16,45 @@ HOW TO USE:
 2. Playtest the game.  Note the transparent button on the lower-right of the game window.
 3. Click the transparent button and note that the game goes fullscreen.
 4. Hit the "escape" key and the game becomes non-fullscreen.
+
+// Customize the general button style
+//!CONFIG button-style-normal (text) "width: 21px; height: 21px; padding: 0; margin:0; background-color: #888888; border: 2px outset #888888;"
+
+// Customize the button style while the pointer is hovered over it
+//!CONFIG button-style-hovered (text) "background-color: #ffffff;"
+
+// Customize the button style while the pointer press pressed over it
+//!CONFIG button-style-down (text) "border: 2px inset #888888;"
+
+// Customize the button icon - the visual content within the button.
+//!CONFIG button-icon (text) "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' stroke='currentColor' stroke-linecap='round' fill='none' stroke-width='1.25' width='14' height='14'><path d='M5 2h-3v3'></path><path d='M11 2h3v3'></path><path d='M2 11v3h3'></path><path d='M14 11v3h-3'></path></svg>"
 */
 
-const playerUi = document.getElementById('player');
+const style = document.createElement('style');
+document.head.append(style);
+style.id = 'fullscreenButtonSytles';
+style.innerHTML = `
+	#fullscreen-button        { position: absolute; right: 2px; bottom: 2px; opacity: 50%; }
+	#fullscreen-button        { ${FIELD(CONFIG, 'button-style-normal', 'text')} }
+	#fullscreen-button:hover  { ${FIELD(CONFIG, 'button-style-hovered', 'text')} }
+	#fullscreen-button:active { ${FIELD(CONFIG, 'button-style-down', 'text')} }
+`;
+const playbackCanvas = document.getElementById('player-canvas');
 const fullscreenButton = document.createElement('button');
-playerUi.append(fullscreenButton);
-fullscreenButton.id = 'btnToggleFullscreen';
-fullscreenButton.setAttribute('style', 'position:absolute;width:21px;height:21px;right:2px;bottom:2px;padding:0;opacity:40%;');
-fullscreenButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" stroke="currentColor" stroke-linecap="round" fill="none" stroke-width="1.25" width="14" height="14"><path d="M5 2h-3v3"></path><path d="M11 2h3v3"></path><path d="M2 11v3h3"></path><path d="M14 11v3h-3"></path></svg>`;
+playbackCanvas.parentElement.append(fullscreenButton);
+fullscreenButton.id = 'fullscreen-button';
+fullscreenButton.title = 'toggle fullscreen mode';
+fullscreenButton.innerHTML = FIELD(CONFIG, 'button-icon', 'text');
 fullscreenButton.addEventListener('click', () => {
-	const canvas = document.getElementById('player-canvas');
-	if (!canvas) return;
 	if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
-		if (canvas.requestFullscreen) {
-			canvas.requestFullscreen();
+		if (playbackCanvas.requestFullscreen) {
+			playbackCanvas.requestFullscreen();
 		} else if (document.documentElement.msRequestFullscreen) {
-			canvas.msRequestFullscreen();
+			playbackCanvas.msRequestFullscreen();
 		} else if (document.documentElement.mozRequestFullScreen) {
-			canvas.mozRequestFullScreen();
+			playbackCanvas.mozRequestFullScreen();
 		} else if (document.documentElement.webkitRequestFullscreen) {
-			canvas.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+			playbackCanvas.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
 		}
 	} else {
 		// eslint-disable-next-line no-lonely-if
