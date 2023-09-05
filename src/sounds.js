6703c1d67a69af584b6b7f49527b4512249a0462
Jon Heard <jon.heard.tutor@gmail.com>
2023-09-05 03:19:56 -0400

    plugin update - sounds - plugin now replaces bipsi's PLAY_MUSIC system instead of working alongside it. - comment updates - Update for plugin now replacing PLAY_MUSIC - comment updates - Removed tab indentation in comments - comment updates - polished prose - comment updates - Added HOW TO for "touch-sound" with library sound

--- src/sounds.js
+++ src/sounds.js
@@ -1,46 +1,73 @@
 /**
 🔊
 @file sounds
-@summary Sound effects from audio files (mp3, wav).  Also adds volume control for sounds and music.
+@summary Sound effects and music from audio files (mp3, wav).
 @license MIT
 @author Violgamba (Jon Heard)
 
 @description
-Work with sound effect files (mp3, wav) in a manner akin to how Bipsi handles music.
+A basic system for sound effects and music from audio files (mp3, wav).  Use this instead of bipsi's
+PLAY_MUSIC system for more control over the music.
 
-- Plugin adds a new field behavior.  An event that has a "touch-sound" file field that contains a
-	sound file (mp3 or wav), will play that sound file when it is touched.
+- touch-sound field - This plugin adds the "touch-sound" behavior field to make an event play a
+  sound whenever it is touched.
+  The touch-sound field can be a file field with a sound file (mp3 or wav) that is played on touch.
+  The touch-sound field can be a text field containing the name of a sound file field in the game's
+  library event.  When touched the sound is played from the library event.
 
-- Plugin adds the following script functions:
-	- PLAY_SOUND(sound, channel, looped) - Plays a sound.
-		- "sound" - Indicates which sound to play.  This can be a string with the name of a field
-		contained in the current event or the library event, or the result of a call to
-		"FIELD_OR_LIBRARY(name)".
-		- "channel" - An optional string parameter that defaults to "main".  For basic use, leave
-		it null.  In this case, if you play a sound while a second one is already playing, the
-		second sound is interrupted and stops.  If you need to play multiple sounds simultaneously
-		(example: "sound effects" vs "ambient") use different channel names for different sounds.
-		This will cause the sounds to play on separate sound objects so they will not interrupt
-		each other.
-		- "looped" - An optional boolean parameter that defaults to false.  If true, signifies that
-		this sound should repeat indefinitely, or until stopped with "STOP_SOUND()" or interrupted
-		by a different sound played on the same channel.
-	- STOP_SOUND(channel) - Stops a playing sound playing.
-		- "channel" - An optional string parameter.  If specified, the sound playing on the given
-		channel is stopped, otherwise all sounds on all channels are stopped.
-	- SET_SOUND_VOLUME(volume, channel) - Set sound volume.
-		- "volume" - The volume to set sound to.  Expects a value from 0.0 to 1.0.
-		- "channel" - An optional string parameter.  If specified, the volume is set only for the
+- This plugin adds the following script functions:
+  - PLAY_SOUND(sound, channel, looped) - Plays a sound.
+    - "sound" - Indicates which sound to play.  This is a string with the name of a field contained
+      in the current event or the library event.  Alternately this can contain the result of a call
+      to FIELD_OR_LIBRARY(name).
+    - "channel" - An optional string parameter that defaults to "main".  For basic use, leave
+      it null for the default.  If you play a sound while a previous sound is already playing, then
+      the previous sound is interrupted and stops.  If you need to play multiple sounds
+      simultaneously, such as playing sound effects while an ambient sound loops, then set this
+      parameter to different names for different sound types (such as "fx" and "ambient").  This
+      will cause the sounds to play on separate channels so they will not interrupt each other.
+    - "looped" - An optional boolean parameter that defaults to false.  If true, signifies that
+      this sound should repeat indefinitely, or until stopped with "STOP_SOUND()" or interrupted
+      by a different sound played on the same channel.  If false, this sound will play only once.
+  - STOP_SOUND(channel) - Stops a playing sound from playing.
+    - "channel" - An optional string parameter.  If specified, the sound playing on the given
+      channel is stopped.  If not specified, all sounds on all channels are stopped.
+  - SET_SOUND_VOLUME(volume, channel) - Set the volume for one channel, or for all channels.
+    - "volume" - The volume to set.  Expects a value from 0.0 to 1.0.
+    - "channel" - An optional string parameter.  If specified, the volume is set only for the
 		given channel.  If not specified, the volume is set for ALL channels.
-	- SET_MUSIC_VOLUME(volume) - Set music volume.
-		- "volume" - The volume to set music to.  Expects a value from 0.0 to 1.0.
+  - ADD_ON_SOUND_END(callback, channel, runOnce) - Run code when a playing sound reaches its end.
+	Note - This WILL trigger if a sound is looped.  If so, it triggers just before the sound loops.
+    Note - This will NOT trigger if a sound is interrupted.
+	Note - This returns an object that can be passed to REMOVE_ON_SOUND_END.
+    - "callback" - A JavaScript function containing the code to run.  The function is passed a
+      "name" argument containing the name of the sound that has ended.
+    - "channel" - An optional string parameter that defaults to "main".  Only sounds played on this
+      channel will trigger the given jsFunction.
+    - "runOnce" - An optional boolean parameter that defaults to false.  If true, then only the next
+      sound end will trigger the given jsFunction.  If false, all subsequent sound ends will trigger
+      the given jsFunction.
+  - REMOVE_ON_SOUND_END(toStop) - Removes code which triggers when sounds end, i.e. code that was
+    passed to ADD_ON_SOUND_END().
+    - "soundEndToStop" - An object returned from a call to ADD_ON_SOUND_END which represents the
+      code we want to stop triggering.
 
 
 HOW TO USE - EVENT TOUCH TRIGGERS AUDIO - SIMPLE:
 1. Import this plugin into your game.
-2. Pick an event to trigger audio from and add an audio file (mp3 or wav) to it as a file field
-	named "touch-sound".
+2. Pick an event to trigger audio from and add an sound file (mp3 or wav) to it as a file field
+   named "touch-sound".
 3. Run the game and touch the event of step 2.  Notice that the sound plays.
+
+
+HOW TO USE - EVENT TOUCH TRIGGERS AUDIO - SIMPLE, USING A GAME LIBRARY:
+1. Import this plugin into your game.
+2. Add a library event by adding a tag to an event named "is-library".
+3. Add a sound file (mp3 or wav) to the library event of step 2 as a file field.  Name it something
+   appropriate.
+4. Pick an event to trigger audio from and add a text field named "touch-sound".  Set the text field
+   to the name of the field of step 3.
+5. Run the game and touch the event of step 2.  Notice that the sound of step 3 plays.
 
 
 HOW TO USE - EVENT TOUCH TRIGGERS AUDIO - INTERMEDIATE:
@@ -54,7 +81,7 @@ HOW TO USE - EVENT TOUCH TRIGGERS AUDIO - INTERMEDIATE:
 NOTE - Adding the file field of step 2 to a library event, instead of the trigger event, also works.
 
 
-HOW TO USE - EVENT TOUCH TRIGGERS AUDIO - ADVANCED (LIKE MUSIC IS DONE):
+HOW TO USE - EVENT TOUCH TRIGGERS AUDIO - ADVANCED:
 1. Import this plugin into your game.
 2. Pick an event to trigger audio from and add an audio file (mp3 or wav) to it as a "file" field
 	with whatever name you wish.
@@ -70,9 +97,6 @@ NOTE - Adding the file field of step 2 to a library event, instead of the trigge
 
 // The starting volume for sound
 //!CONFIG default-sound-volume (text) "1.0"
-
-// The starting volume for music
-//!CONFIG default-music-volume (text) "1.0"
 */
 
 const DEFAULT_SOUND_CHANNEL = 'main';
@@ -80,7 +104,6 @@ const DEFAULT_SOUND_CHANNEL = 'main';
 wrap.after(window, 'start', () => {
 	window.PLAYBACK.soundChannels = {};
 	window.PLAYBACK.defaultSoundVolume = parseFloat(FIELD(CONFIG, 'default-sound-volume', 'text')) || 0;
-	window.PLAYBACK.music.volume = parseFloat(FIELD(CONFIG, 'default-music-volume', 'text')) || 0;
 });
 
 BipsiPlayback.prototype.playSound = function playSound(sound, channel, looped) {
@@ -153,18 +176,6 @@ BipsiPlayback.prototype.setSoundVolume = function setSoundVolume(volume, channel
 };
 SCRIPTING_FUNCTIONS.SET_SOUND_VOLUME = function SET_SOUND_VOLUME(volume, channel) {
 	this.PLAYBACK.setSoundVolume(volume, channel);
-};
-
-BipsiPlayback.prototype.setMusicVolume = function setMusicVolume(volume) {
-	if (Number.isNaN(parseFloat(volume))) {
-		console.log(`Invalid music volume: "${volume}".`);
-	}
-	volume = Math.min(Math.max(parseFloat(volume), 0), 1);
-
-	this.music.volume = volume;
-};
-SCRIPTING_FUNCTIONS.SET_MUSIC_VOLUME = function SET_MUSIC_VOLUME(volume) {
-	this.PLAYBACK.setMusicVolume(volume);
 };
 
 const BEHAVIOUR_TOUCH_SOUND = `
